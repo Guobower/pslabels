9ca5a6e80f57bd31a3e01007ab46ea5932508da6
Antonio A. Carrillo <peluko@peluko.net>
2018-06-02 10:39:38 +0200

    Etiquetas con color

--- reports/pslabels_reports.py
+++ reports/pslabels_reports.py
@@ -8,14 +8,32 @@ class PSLabelsReport1(models.AbstractModel):
     labels_per_page = 18
     template_name = 'pslabels.report_pslabels_view_7024'
 
+    def expand_product(self, wizard_product):
+        expanded = self.env['product.product'].browse(wizard_product['product_id']);
+
+        # talla_attr = next((a for a in expanded.attribute_value_ids if a.attribute_id.name == 'Talla'), None)
+        # expanded.talla = talla_attr.name if talla_attr else '*'
+
+        expanded.size = ''
+        expanded.style = ''
+        for a in expanded.attribute_value_ids:
+            if a.attribute_id.name == 'Talla':
+                expanded.exp_size = '- T' + a.name
+            elif a.attribute_id.name == 'Color Etiqueta':
+                expanded.exp_color = 'padding-top: 1mm; width: 5mm; height: 5mm; border-radius: 50%%; display: inline-block; background-color: %s;' % a.name[:7]
+            else:
+                expanded.exp_style = expanded.style + a.name + ' '
+
+        return expanded
+
     @api.model
     def render_html(self, docids, data=None):
         data = data if data is not None else {}
         docids = data.get('ids')
 
-        product_qty_lists = map(lambda p: [self.env['product.product'].browse(p['product_id'])] * p['qty'], data['product_qty_pairs'])
+        product_qty_lists = map(lambda p: [self.expand_product(p)] * p['qty'], data['product_qty_pairs'])
         products = ([{}] * data['to_skip']) + reduce(list.__add__, product_qty_lists)
-        pages = (products[i:i + self.labels_per_page] for i in xrange(0, len(products), self.labels_per_page))
+        pages = (products[i:i + self.labels_per_page] for i in range(0, len(products), self.labels_per_page))
         pre_product_name = data['pre_product_name']
 
         docargs = {
